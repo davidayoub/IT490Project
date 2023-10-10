02c0816b941a9b448e22f9a536968f79b2605f04
Luis V <89930344+alejjuuu@users.noreply.github.com>
2023-10-10 14:35:20 -0400

    new nabar and headlinks

--- partials/nav.php
+++ partials/nav.php
@@ -22,3 +22,41 @@ if (($localWorks && $domain == "localhost") || $domain != "localhost") {
 session_start();
 require_once(__DIR__ . "/../lib/functions.php");
 ?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Register</title>
+    <link rel="icon" href="path_to_favicon.ico">
+    <meta name="description" content="Description of your website.">
+    <meta name="keywords" content="keyword1, keyword2, keyword3">
+    <meta name="author" content="Your Name or Company Name">
+
+    <!-- Tailwind CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+
+    <!-- DaisyUI CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/daisyui/dist/full.css" rel="stylesheet">
+
+    <!-- DaisyUI Autumn Theme CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@1.10.2/themes/autumn.min.css" rel="stylesheet">
+
+    <script src="js/form_validation.js"></script>
+
+    <!-- If you want your site to be displayed nicely when shared on social platforms -->
+    <!-- Open Graph tags -->
+    <meta property="og:title" content="Title Here">
+    <meta property="og:description" content="Description Here">
+    <meta property="og:image" content="URL to your image">
+    <meta property="og:url" content="URL of your website">
+
+    <!-- Twitter Card tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Title Here">
+    <meta name="twitter:description" content="Description Here">
+    <meta name="twitter:image" content="URL to your image">
+</head>
