f4fe5195cddd69a6fa4968a3092b46aea1eade47
davidayoub <da44@njit.edu>
2023-11-02 17:53:52 -0400

    saving

--- public_html/consumerpractice.php
+++ public_html/consumerpractice.php
@@ -1,242 +1,113 @@
 <?php
-require(__DIR__."/../partials/nav.php");
-require_once(__DIR__.'/rabbitmqphp_example/rabbitMQLib.inc');
 require_once __DIR__ . '/vendor/autoload.php';
+require_once(__DIR__ . "/../lib/functions.php");
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
+// RabbitMQ server configuration
+$rabbitmqHost = 'localhost';
+$rabbitmqPort = 5672;
+$rabbitmqUser = 'test';
+$rabbitmqPass = 'test';
+$rabbitmqVhost = 'it490';
 
-    $rabbitmqHost = 'localhost';
-    $rabbitmqPort = 5672;
-    $rabbitmqUser = 'test';
-    $rabbitmqPass = 'test';
-    $rabbitmqVhost = 'it490';
-    
-    // Create a connection to RabbitMQ
-    $connection = new AMQPStreamConnection($rabbitmqHost, $rabbitmqPort, $rabbitmqUser, $rabbitmqPass, $rabbitmqVhost);
-    
-    // Create a channel
-    $channel = $connection->channel();
-    
-    // Declare a queue to send the data
-    $queueName = 'testQueue';
-    $channel->queue_declare($queueName, false, true, false, false);
+// MySQL database configuration
+$mysqlHost = 'localhost';
+$mysqlUser = 'TestUser';
+$mysqlPass = '12345';
+$mysqlDb = 'form';
 
+// Create a connection to RabbitMQ
+$connection = new AMQPStreamConnection($rabbitmqHost, $rabbitmqPort, $rabbitmqUser, $rabbitmqPass,$rabbitmqVhost);
 
+// Create a channel
+$channel = $connection->channel();
 
+// Declare the queue to consume messages
+$queueName = 'testQueue';
+$channel->queue_declare($queueName, false, true, false, false);
 
+echo " [*] Waiting for messages. To exit, press Ctrl+C\n";
 
+$callback = function ($message) use ($mysqlHost, $mysqlUser, $mysqlPass, $mysqlDb) {
+    $data = json_decode($message->body, true);
 
+    if ($data['type'] === 'register') {
+        // Insert the registration data into the MySQL table
+        $mysqli = new mysqli($mysqlHost, $mysqlUser, $mysqlPass, $mysqlDb);
 
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (isset($_POST["login"])) {
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-
-        $request = array();
-        $request['type'] = "login";
-        $request['username'] = $username;
-        $request['password'] = $password;
-
-        $email = se($_POST, "username", "", false);
-        $password = se($_POST, "password", "", false);
-        $hasErrors = false;
-        if (empty($email)) {
-            echo("Username or email must be set");
-            $hasErrors = true;
-        } elseif (str_contains($email, "@")) {
-            $email = sanitize_email($email);
-            if (!is_valid_email($email)) {
-                echo("Invalid email address");
-                $hasErrors = true;
-            }
-        } elseif (!preg_match('/^[a-z0-9_-]{3,30}$/i', $email)) {
-            echo("Username must only be alphanumeric and can only contain - or _");
-            $hasErrors = true;
-        }
-        if (empty($password)) {
-            echo("Password must be set");
-            $hasErrors = true;
-        } elseif (strlen($password) < 8) {
-            echo("Password must be at least 8 characters");
-            $hasErrors = true;
+        if ($mysqli->connect_error) {
+            die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
         }
 
-        try {
-            // RabbitMQ Below
-           // $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-            // $response = $client->publish($request);
-           // $response = $client->send_request($request);
-            //var_dump($response);
+        $email = $mysqli->real_escape_string($data['email']);
+        $username = $mysqli->real_escape_string($data['username']);
+        $password = $mysqli->real_escape_string($data['password']);
 
-           // $_SESSION["user"]["roles"] = $response ?: [];
+       // $query = "INSERT INTO users (email, username, password) VALUES ('$email', '$username', '$password')";
+
+       $hash = password_hash($password, PASSWORD_BCRYPT);
+       $db = getDB();
+       $stmt = $db->prepare("INSERT INTO users (email, password, username) VALUES(:email, :password, :username)");
+       try {
+           $stmt->execute([":email" => $email, ":password" => $hash, ":username" => $username]);
+           echo("Successfully registered!");
+           echo " [x] Data inserted into MySQL table\n";
+           echo " [x] Received Email: ", $data['email'], "\n";
+           echo " [x] Received Username: ", $data['username'], "\n";
+           echo " [x] Received Password: ", $data['password'], "\n";
+        echo " \n" ;
+       } catch (Exception $e) {
+          // users_check_duplicate($e->$errorInfo);
+       }
+
+       // if ($mysqli->query($query) === true) {
+           // echo " [x] Data inserted into MySQL table\n";
+          //  echo " [x] Received Email: ", $data['email'], "\n";
+         // echo " [x] Received Username: ", $data['username'], "\n";
+         //   echo " [x] Received Password: ", $data['password'], "\n";
+          //  echo " \n" ;
+       // } else {
+        //    echo " [x] Error: " . $query . "\n" . $mysqli->error;
+       // }
+
+        $mysqli->close();
+    } elseif ($data['type'] === 'login') {
+        // Handle login data
+        $usernames = $data['username'];
+        $passwords= $data['password'];
+
+        // Check login credentials against the database (modify this part as per your authentication logic)
+        $mysqli = new mysqli($mysqlHost, $mysqlUser, $mysqlPass, $mysqlDb);
+
+        if ($mysqli->connect_error) {
+            die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+        }
+
+        $username = $mysqli->real_escape_string($usernames);
+        $password = $mysqli->real_escape_string($passwords);
+
+        $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+        $result = $mysqli->query($query);
+
+        if ($result->num_rows > 0) {
+            // Login is successful
+            // Set a session or cookie to indicate that the user is logged in
+            echo " \n" ;
+            echo " [x] Login is successful\n";
+
             redirect("home.php");
-        } catch (Exception $e) {
-            echo(var_export($e, true));
-        }
-
-        //Starting from here everything was commented out
-        $db = getDB();
-        $stmt = $db->prepare("SELECT id, username, email, password from users where email = :email or username = :email");
-        try {
-            $r = $stmt->execute([":email" => $email]);
-            if ($r && $user = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                if (password_verify($password, $user["password"])) {
-                    unset($user["password"]);
-                    $_SESSION["user"] = $user;
-                    // Just in case there is a session admin
-                    //$stmt = $db->prepare("SELECT Roles.name FROM Roles JOIN UserRoles on Roles.id = UserRoles.role_id where UserRoles.user_id = :user_id and Roles.is_active = 1 and UserRoles.is_active = 1");
-                    // $stmt->execute([":user_id" => $user["id"]]);
-                    // $_SESSION["user"]["roles"] = $stmt->fetchAll(PDO::FETCH_ASSOC) ?: [];
-
-                    // RabbitMQ Below
-                    //$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-                    //$response = $client->send_request($request);
-                   // var_dump($response);
-
-                            //$dataJson = json_encode($request);
-    
-                            // Prepare the message
-                           //$message = new AMQPMessage($request);
-                        
-                            //Publish the message to the combined data queue
-                            //$channel->basic_publish($request);
-
-
-                    redirect("home.php");
-
-                } else {
-                    echo("Invalid password");
-                }
-            } else {
-                echo("Email not found");
-            }
-        } catch (Exception $e) {
-            echo(var_export($e, true));
-        }
-        //From here and up, everything was commented out
-
-
-        
-        //$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-       // $response = $client->send_request($request);
-
-        //var_dump($response);
-        
-    }
-
-    // Similarly, you can handle registration here
-    if (isset($_POST["register"])) {
-        $email = $_POST["email"];
-        $username = $_POST["username"];
-        $password = $_POST["password"];
-        $confirm = $_POST["confirm"];
-
-        $request = array();
-        $request['type'] = "register";
-        $request['email'] = $email;
-        $request['username'] = $username;
-        $request['password'] = $password;
-        $request['confirm'] = $confirm;
-
-        if (!is_valid_email($email)) {
-            flash("Invalid email address", "danger");
-            $hasError = true;
-        }
-        if (!preg_match('/^[a-z0-9_-]{3,16}$/i', $username)) {
-            echo("Username must only be alphanumeric and can only contain - or _");
-            $hasError = true;
-        }
-        if (empty($password)) {
-            flash("password must not be empty", "danger");
-            $hasError = true;
-        }
-        if (empty($confirm)) {
-            flash("Confirm password must not be empty", "danger");
-            $hasError = true;
-        }
-        if (strlen($password) < 8) {
-            flash("Password too short", "danger");
-            $hasError = true;
-        }
-        if (
-            strlen($password) > 0 && $password !== $confirm
-        ) {
-            flash("Passwords must match", "danger");
-            $hasError = true;
-        }
-
-
-
-        $hash = password_hash($password, PASSWORD_BCRYPT);
-        $db = getDB();
-        $stmt = $db->prepare("INSERT INTO users (email, password, username) VALUES(:email, :password, :username)");
-        try {
-            $stmt->execute([":email" => $email, ":password" => $hash, ":username" => $username]);
+        } else {
+            // Login failed, you can handle this scenario as needed
             echo " \n" ;
-            echo " [x] Data inserted into MySQL table\n";
-            echo " [x] Received Email: ", $data['email'], "\n";
-            echo " [x] Received Username: ", $data['username'], "\n";
-            echo " [x] Received Password: ", $data['password'], "\n";
-            echo " \n" ;
-        } catch (Exception $e) {
-           // users_check_duplicate($e->$errorInfo);
+            echo " [x] Login failed\n";
         }
-        
 
-
-        //$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-        //$response = $client->send_request($request);
-
-       // var_dump($response);
-
-        //Incase client ^ does not work
-        //$dataJson = json_encode($request);
-    
-        // Prepare the message
-      // $message = new AMQPMessage($request);
-    
-        //Publish the message to the combined data queue
-       // $channel->basic_publish($request);
-
-
-
-
-
-
+        $mysqli->close();
     }
-}
+};
 
-
-function se($v, $k = null, $default = "", $isEcho = true)
-{
-    if (is_array($v) && isset($k) && isset($v[$k])) {
-        $returnValue = $v[$k];
-    } else if (is_object($v) && isset($k) && isset($v->$k)) {
-        $returnValue = $v->$k;
-    } else {
-        $returnValue = $v;
-        //added 07-05-2021 to fix case where $k of $v isn't set
-        //this is to kep htmlspecialchars happy
-        if (is_array($returnValue) || is_object($returnValue)) {
-            $returnValue = $default;
-        }
-    }
-    if (!isset($returnValue)) {
-        $returnValue = $default;
-    }
-    if ($isEcho) {
-        //https://www.php.net/manual/en/function.htmlspecialchars.php
-        echo htmlspecialchars($returnValue, ENT_QUOTES);
-    } else {
-        //https://www.php.net/manual/en/function.htmlspecialchars.php
-        return htmlspecialchars($returnValue, ENT_QUOTES);
-    }
-}
-
-
-
+// Consume messages from the queue
 $channel->basic_consume($queueName, '', false, true, false, false, $callback);
 
 while (count($channel->callbacks)) {
@@ -246,23 +117,4 @@ while (count($channel->callbacks)) {
 // Close the channel and connection (will not be reached in this example)
 $channel->close();
 $connection->close();
-
-
-
-
-
-
-
-
-
-
 ?>
-
-
-
-
-
-
-
-}
-
