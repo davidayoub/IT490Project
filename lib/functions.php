34bacae7620fcf68f5fbccfba0dab3ae192fb858
alejjuuu <lav29@njit.edu>
2023-11-02 11:30:33 -0400

    new changes on the navbar page

--- lib/functions.php
+++ lib/functions.php
@@ -180,18 +180,19 @@ function validate_message($message)
 
 
 function redirect($path)
-{ //header headache
-    //https://www.php.net/manual/en/function.headers-sent.php#90160
-    /*headers are sent at the end of script execution otherwise they are sent when the buffer reaches it's limit and emptied */
-    if (!headers_sent()) {
-        //php redirect
-        die(header("Location: " . get_url($path)));
+{
+    if (php_sapi_name() != 'cli') {
+        if (!headers_sent()) {
+            header("Location: " . get_url($path));
+            exit();
+        } else {
+            echo "<script>window.location.href='" . get_url($path) . "';</script>";
+            echo "<noscript><meta http-equiv=\"refresh\" content=\"0;url=" . get_url($path) . "\"/></noscript>";
+            exit();
+        }
+    } else {
+        echo "This script must be run in a web browser for redirection to work.\n";
     }
-    //javascript redirect
-    echo "<script>window.location.href='" . get_url($path) . "';</script>";
-    //metadata redirect (runs if javascript is disabled)
-    echo "<noscript><meta http-equiv=\"refresh\" content=\"0;url=" . get_url($path) . "\"/></noscript>";
-    die();
 }
 
 
--- public_html/rabbitmqphp_example/testRabbitMQServer.php
+++ public_html/rabbitmqphp_example/testRabbitMQServer.php
@@ -1,37 +1,153 @@
-#!/usr/bin/php
 <?php
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
 
-function doLogin($username,$password)
-{
-    // lookup username in databas
-    // check password
-    return true;
-    //return false if not valid
-}
+require(__DIR__.'/../rabbitmqphp_example/path.inc');
+require(__DIR__.'/../rabbitmqphp_example/get_host_info.inc');
+require(__DIR__.'/../rabbitmqphp_example/rabbitMQLib.inc');
+require(__DIR__.'/../../lib/functions.php');
+//require_once('rabbitMQLib.inc');
 
 function requestProcessor($request)
 {
-  echo "received request".PHP_EOL;
-  var_dump($request);
-  if(!isset($request['type']))
-  {
-    return "ERROR: unsupported message type";
-  }
-  switch ($request['type'])
-  {
-    case "login":
-      return doLogin($request['username'],$request['password']);
-    case "validate_session":
-      return doValidate($request['sessionId']);
-  }
-  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+    echo "Received request".PHP_EOL;
+    var_dump($request);
+
+    if(!isset($request['type'])) {
+        return ["status" => "error", "message" => "Unsupported request type"];
+    }
+
+    switch($request['type']) {
+        case "login":
+            return doLogin($request['username'], $request['password']);
+            break;
+        case "register":
+            return doRegister($request['email'], $request['username'], $request['password'], $request['confirm']);
+            break;
+        default:
+            return ["status" => "error", "message" => "Unsupported request type"];
+    }
 }
 
-$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 
-$server->process_requests('requestProcessor');
-exit();
+function doLogin($email, $password){
+    
+    $response = ["status" => "error", "message" => ""];
+    
+    if (empty($email)) {
+        $response["message"] = "Username or email must be set";
+        return $response;
+    }
+    
+    //sanitize
+    if (str_contains($email, "@")) {
+        $email = sanitize_email($email);
+
+        if (!is_valid_email($email)) {
+            $response["message"] = "Invalid email address";
+            return $response;
+        }
+    } else {
+        if (!preg_match('/^[a-z0-9_-]{3,30}$/i', $email)) {
+            $response["message"] = "Username must only be alphanumeric and can only contain - or _";
+            return $response;
+        }
+    }
+
+    if (empty($password) || strlen($password) < 8) {
+        $response["message"] = "Password must be set and at least 8 characters long";
+        return $response;
+    }
+
+    $db = getDB();
+    $stmt = $db->prepare("SELECT id, username, email, password FROM users WHERE email = :email OR username = :email");
+    try {
+        $r = $stmt->execute([":email" => $email]);
+        if ($r) {
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($user) {
+                $hash = $user["password"];
+                if (password_verify($password, $hash)) {
+                    $response["status"] = "success";
+                    $response["user"] = $user;
+                } else {
+                    $response["message"] = "Invalid password";
+                }
+            } else {
+                $response["message"] = "Email not found";
+            }
+        }
+    } catch (Exception $e) {
+        $response["message"] = var_export($e, true);
+    }
+    
+    return $response;
+}
+
+function doRegister($email, $username, $password, $confirm)
+{
+    $response = ["status" => "error", "message" => ""];
+
+    $hasError = false;
+
+    // Check for empty fields
+    if (empty($email)) {
+        $response["message"] = "Email must not be empty";
+        return $response;
+    }
+    if (empty($username)) {
+        $response["message"] = "Username must not be empty";
+        return $response;
+    }
+    if (empty($password)) {
+        $response["message"] = "Password must not be empty";
+        return $response;
+    }
+    if (empty($confirm)) {
+        $response["message"] = "Confirm password must not be empty";
+        return $response;
+    }
+
+    // Sanitize email
+    $email = sanitize_email($email);
+
+    // Validate email and username
+    if (!is_valid_email($email)) {
+        $response["message"] = "Invalid email address";
+        return $response;
+    }
+    if (!preg_match('/^[a-z0-9_-]{3,16}$/i', $username)) {
+        $response["message"] = "Username must only be alphanumeric and can only contain - or _";
+        return $response;
+    }
+
+    // Check password length
+    if (strlen($password) < 8) {
+        $response["message"] = "Password too short";
+        return $response;
+    }
+
+    // Check password confirmation
+    if ($password !== $confirm) {
+        $response["message"] = "Passwords must match";
+        return $response;
+    }
+
+    // If no errors, proceed with database insertion
+    $hash = password_hash($password, PASSWORD_BCRYPT);
+    $db = getDB();
+    $stmt = $db->prepare("INSERT INTO users (email, password, username) VALUES(:email, :password, :username)");
+
+    try {
+        $stmt->execute([":email" => $email, ":password" => $hash, ":username" => $username]);
+        $response["status"] = "success";
+        $response["message"] = "Successfully generated new account.";
+    } catch (Exception $e) {
+        $response["message"] = $e->getMessage();
+    }
+
+    return $response;
+}
+
+
 ?>
+
+
