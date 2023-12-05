218b81f1d748a66d41c7c86e5fabf7d6a3a6c834
alejjuuu <lav29@njit.edu>
2023-12-05 15:44:16 -0500

    Your commit message

--- Dev/partials/banner.php
+++ Dev/partials/banner.php
@@ -1,4 +1,4 @@
-<div class="bg-indigo-600">
+<div class="bg-black">
     <div class="max-w-screen-xl mx-auto px-4 py-3 text-white sm:text-center md:px-8">
         <p class="font-medium">
             We just launched a new version of our library! <a href="javascript:void(0)" class="font-semibold underline duration-150 hover:text-indigo-100 inline-flex items-center gap-x-1">
